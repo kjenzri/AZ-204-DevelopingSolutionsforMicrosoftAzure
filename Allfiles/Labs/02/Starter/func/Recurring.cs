5e4b71f7ca2916633459e1e7aa7aac618a4be396
Karim JENZRI <kjenzri@yahoo.com>
2025-11-14 21:43:11 +0100

    func lab in progress

---
+++ Allfiles/Labs/02/Starter/func/Recurring.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace func;
+
+public class Recurring
+{
+    private readonly ILogger _logger;
+
+    public Recurring(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<Recurring>();
+    }
+
+    [Function("Recurring")]
+    public void Run([TimerTrigger("* * * * * *")] TimerInfo myTimer)
+    {
+        _logger.LogInformation("C# Timer trigger function executed at: {executionTime}", DateTime.Now);
+        
+        if (myTimer.ScheduleStatus is not null)
+        {
+            _logger.LogInformation("Next timer schedule at: {nextSchedule}", myTimer.ScheduleStatus.Next);
+        }
+    }
+}
